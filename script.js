ca5cb9f1ce7cd71497389d71563a2762c451a669
Emmanuel Geldreich <egeldreich@gmail.com>
2025-01-16 16:29:09 +0100

    Now putting all pokemon data in an object, which is in time inserted ordered in an array. This result in the pokemons now being in the right order.
    Utilising promise.all to wait all the requests

--- script.js
+++ script.js
@@ -5,24 +5,68 @@ const entryTemplateEl = document.querySelector("#entry_template");
 
 let pokemonData;
 
-// // POKEDEX URL SEEKER FUNCTION
-// async function getGenerationPokedexEndpoint(inputGeneration) {
-//     // seek the correct pokedex from pokedex list
-//     const response = await fetch("https://pokeapi.co/api/v2/pokedex?limit=100");
-//     const data = await response.json();
+// GET POKEMON INFORMATION
+async function getGenerationPokemonInfo(generationUrl) {
+    // Fetch generation pokedex
+    const response = await fetch(generationUrl);
+    const data = await response.json();
 
-//     // Seek the right generation from data
-//     for (let generation of data.results) {
-//         if (
-//             generation.name == inputGeneration ||
-//             generation.name == `original-${inputGeneration}` ||
-//             generation.name == `${inputGeneration}-central`
-//         ) {
-//             return generation.url; // Return the url
-//         }
-//     }
-//     return null; // Failsafe in case of no result
-// }
+    // Initialize pokemonData array / Or reset for each generation
+    pokemonData = [];
+
+    // GET POKEMON DATAS AND PUT IT IN ARRAY
+    // Using an array to regroup promises in order to user Promise.all later
+    const pokemonPromises = data.pokemon_species.map((pokemon) => {
+        //
+        // Fetch pokemon data
+        // Get Pokemon Endpoint by removing -species from url (not the same informations are retrieved)
+        let pokemonUrl = pokemon.url.replace("-species", "");
+        // RETURN needed to answer Promise.all
+        return fetch(pokemonUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                // Get relevant informations and set temporary object
+                let currentPokemon = {
+                    name: data.name,
+                    id: data.id,
+                    type: data.types[0].type.name,
+                    src: data.sprites.front_default,
+                };
+                // Push object in array at a determined index
+                pokemonData[data.id - 1] = currentPokemon;
+            });
+    });
+
+    // Await all pending promises
+    await Promise.all(pokemonPromises);
+
+    console.log(pokemonData);
+
+    pokemonData.forEach((pokemon) => {
+        console.log("test");
+        // Clone template
+        let templateClone = document.importNode(entryTemplateEl.content, true);
+        // Select template elements
+        let entry = templateClone.querySelector(".entry");
+        let name = templateClone.querySelector(".name");
+        let id = templateClone.querySelector(".id");
+        let type = templateClone.querySelector(".type");
+        let img = templateClone.querySelector(".img");
+
+        // Input relevant data
+        name.textContent = pokemon.name;
+        id.textContent = pokemon.id;
+        type.textContent = pokemon.type;
+        img.src = pokemon.src;
+
+        // Add relevant classes
+        entry.classList.add(pokemon.type);
+
+        pokedexEl.appendChild(templateClone);
+    });
+
+    // console.log(pokemonData);
+}
 
 // GET REGION POKEDEX
 async function getGenerationPokedex() {
@@ -41,44 +85,8 @@ async function getGenerationPokedex() {
         generationBtn.addEventListener("click", async () => {
             // Clean DOM
             pokedexEl.innerHTML = "";
-            // Set up Pokemon Data
-            pokemonData = [];
-
-            // Fetch generation pokedex
-            const response = await fetch(generation.url);
-            // console.log(generation.url);
-            const data = await response.json();
-
-            data.pokemon_species.forEach((pokemon) => {
-                // Fetch pokemon data
-                let pokemonUrl = pokemon.url.replace("-species", "");
-                fetch(pokemonUrl)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data);
-
-                        // Clone template
-                        let templateClone = document.importNode(
-                            entryTemplateEl.content,
-                            true
-                        );
-                        // Select template elements
-                        let entry = templateClone.querySelector(".entry");
-                        let name = templateClone.querySelector(".name");
-                        let nb = templateClone.querySelector(".nb");
-                        let type = templateClone.querySelector(".type");
-                        let img = templateClone.querySelector(".img");
-
-                        // Input relevant data
-                        entry.classList.add(data.types[0].type.name, data.id);
-                        name.textContent = data.name;
-                        nb.textContent = data.id;
-                        type.textContent = data.types[0].type.name;
-                        img.src = data.sprites.front_default;
-
-                        pokedexEl.appendChild(templateClone);
-                    });
-            });
+            // Get and set up Pokemon Data, then fill up DOM
+            getGenerationPokemonInfo(generation.url);
         });
     });
 }
