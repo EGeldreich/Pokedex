8c14d1583d3e7c97fb8f96ca94072133e93b9b91
Emmanuel Geldreich <egeldreich@gmail.com>
2025-01-24 11:59:26 +0100

    Reworked styling. Added second type.

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let pokemonData;
 function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
+function formatNumber(nb){
+    return '#' + nb.toString().padStart(3, '0');
+}
 // GET POKEMON INFORMATION
 async function getGenerationPokemonInfo(generationUrl) {
     // Fetch generation pokedex
@@ -31,10 +34,12 @@ async function getGenerationPokemonInfo(generationUrl) {
                 // Get relevant informations and set temporary object
                 let currentPokemon = {
                     name: data.name,
-                    id: data.id,
+                    id: formatNumber(data.id),
                     type: data.types[0].type.name,
+                    type2: data.types.length > 1 ? data.types[1].type.name : null,
                     src: data.sprites.front_default,
                 };
+
                 // Push object in array at a determined index
                 pokemonData[data.id - 1] = currentPokemon;
             });
@@ -43,10 +48,7 @@ async function getGenerationPokemonInfo(generationUrl) {
     // Await all pending promises
     await Promise.all(pokemonPromises);
 
-    console.log(pokemonData);
-
     pokemonData.forEach((pokemon) => {
-        console.log("test");
         // Clone template
         let templateClone = document.importNode(entryTemplateEl.content, true);
         // Select template elements
@@ -54,21 +56,66 @@ async function getGenerationPokemonInfo(generationUrl) {
         let name = templateClone.querySelector(".name");
         let id = templateClone.querySelector(".id");
         let type = templateClone.querySelector(".type");
+        let type2 = templateClone.querySelector(".type2");
         let img = templateClone.querySelector(".img");
 
         // Input relevant data
         name.textContent = capitalizeFirstLetter(pokemon.name);
         id.textContent = pokemon.id;
         type.textContent = pokemon.type;
+        type2.textContent = pokemon.type2;
         img.src = pokemon.src;
 
         // Add relevant classes
         entry.classList.add(pokemon.type);
 
         pokedexEl.appendChild(templateClone);
-    });
 
-    // console.log(pokemonData);
+    });
+    // HANDLE COLORS
+    let colors = {
+        normal: '#A8A77A80',
+        fire: '#EE813080',
+        water: '#6390F080',
+        electric: '#F7D02C80',
+        grass: '#7AC74C80',
+        ice: '#96D9D680',
+        fighting: '#C22E2880',
+        poison: '#A33EA180',
+        ground: '#E2BF6580',
+        flying: '#A98FF380',
+        psychic: '#F9558780',
+        bug: '#A6B91A80',
+        rock: '#B6A13680',
+        ghost: '#73579780',
+        dragon: '#6F35FC80',
+        dark: '#70574680',
+        steel: '#B7B7CE80',
+        fairy: '#D685AD80',
+    };
+
+    for(color in colors) {
+        let colorCards = document.querySelectorAll(`.${color}`);
+        
+        colorCards.forEach(card => {
+            card.querySelector('.type').style.backgroundColor = colors[color].slice(0, -2);
+
+            card.style.backgroundColor = colors[color];
+        })
+    }
+    let secondTypeEl = document.querySelectorAll('.type2');
+    secondTypeEl.forEach(type => {
+
+    let typeTxt = type.textContent;
+        for(color in colors){
+            if(color == typeTxt){
+                console.log('salut');
+                type.style.backgroundColor = colors[color].slice(0, -2);
+            }
+        }
+
+
+    })
 }
 
 // GET REGION POKEDEX
@@ -93,6 +140,10 @@ async function getGenerationPokedex() {
         });
     });
 }
+
+
+// HANDLE COLOR OF CARDS
+
 
 // ON LOAD
 getGenerationPokedex();
